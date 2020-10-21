bfbfbfcc79d7d23a64eb5b4856459c4a7c786ae9
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2020-10-21 23:44:45 +0300

    added hackmd codimd chart

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ helm upgrade --install nginx-ingress -n clusterwide ingress-nginx/ingress-nginx 
 helm upgrade --install certs -n clusterwide certs/certs -f certs/values.d/limnad.yaml -f certs/values.d/limnad.secret.yaml --atomic --debug
 helm upgrade --install postgresql -n db bitnami/postgresql -f postgresql/values.d/limnad.yaml -f postgresql/values.d/limnad.secret.yaml --atomic --debug
 helm upgrade --install chat -n phantom mattermost/mattermost-team-edition -f mattermost/values.d/limnad.yaml --atomic --debug
+helm upgrade --install -n phantom hackmd stable/hackmd -f hackmd/values.d/limnad.yaml -f hackmd/values.d/limnad.secret.yaml --atomic --debug
 
 helm upgrade --install nasdaq -n stocks stocks-nasdaq-crawler -f stocks-nasdaq-crawler/values.d/limnad.secret.yaml
 helm upgrade --install kix -n phantom website-kix-co-il
