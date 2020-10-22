a9b2789c50aecd8983918e982b40b332b70dc0ef
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2020-10-22 13:20:41 +0300

    archive channels display in mattermost

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ helm upgrade --install oauth2-proxy -n clusterwide stable/oauth2-proxy -f oauth2
 helm upgrade --install nginx-ingress -n clusterwide ingress-nginx/ingress-nginx -f nginx-ingress/values.d/limnad.yaml -f nginx-ingress/values.d/limnad.secret.yaml --atomic --debug
 helm upgrade --install certs -n clusterwide certs/certs -f certs/values.d/limnad.yaml -f certs/values.d/limnad.secret.yaml --atomic --debug
 helm upgrade --install postgresql -n db bitnami/postgresql -f postgresql/values.d/limnad.yaml -f postgresql/values.d/limnad.secret.yaml --atomic --debug
-helm upgrade --install chat -n phantom mattermost/mattermost-team-edition -f mattermost/values.d/limnad.yaml --atomic --debug
+helm upgrade --install chat -n phantom mattermost -f mattermost/values.d/limnad.yaml -f  mattermost/values.d/limnad.secret.yaml --atomic --debug
 helm upgrade --install -n phantom hackmd stable/hackmd -f hackmd/values.d/limnad.yaml -f hackmd/values.d/limnad.secret.yaml --atomic --debug
 
 helm upgrade --install nasdaq -n stocks stocks-nasdaq-crawler -f stocks-nasdaq-crawler/values.d/limnad.secret.yaml
