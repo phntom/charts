1494e96f0f03a471ddd4d07f33774d920d81e7e9
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2021-03-25 20:21:19 +0200

    moved minio to s3 as a gateway, updated keeweb and started upgrading mindav

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ helm create namespace db
 helm create namespace phantom
 helm create namespace stocks
 
+helm upgrade --install prometheus -n clusterwide prometheus-community/kube-prometheus-stack -f kube-prometheus-stack/values.d/limnad.yaml -f kube-prometheus-stack/values.d/limnad.secret.yaml --atomic --debug
+helm upgrade --install nginx-ingress -n clusterwide ingress-nginx/ingress-nginx -f nginx-ingress/values.d/limnad.yaml -f nginx-ingress/values.d/limnad.secret.yaml --atomic --debug
+helm upgrade --install certs -n clusterwide certs/certs -f certs/values.d/limnad.yaml -f certs/values.d/limnad.secret.yaml --atomic --debug
+
 ### from chartmuseum
 helm upgrade --install chartmuseum -n phantom phntom/chartmuseum -f chartmuseum/values.d/limnad.yaml -f chartmuseum/values.d/limnad.secret.yaml --atomic --debug
 ### or directly from the folder
@@ -21,18 +25,17 @@ helm upgrade --install matterircd-prod -n chat phntom/matterircd --atomic --debu
 helm upgrade --install website-web-nix -n chat phntom/website-web-nix --atomic --debug -f website-web-nix/values.d/limnad.secret.yaml
 helm upgrade --install website-kix -n chat phntom/website-kix-co-il --atomic --debug
 
+helm upgrade --install minio -n db bitnami/minio -f minio/values.d/limnad.yaml -f minio/values.d/limnad.secret.yaml --debug --atomic
 helm upgrade --install docker -n db phntom/docker-registry -f docker-registry/values.d/limnad.yaml -f docker-registry/values.d/limnad.secret.yaml --debug --atomic
 
 helm upgrade --install nasdaq -n stocks phntom/stocks-nasdaq-crawler -f stocks-nasdaq-crawler/values.d/limnad.secret.yaml
 
+helm upgrade --install pwd-mindav -n phantom mindav -f mindav/values.d/limnad.secret.yaml --debug --atomic
+
 ###
 
-helm upgrade --install prometheus -n clusterwide prometheus-community/kube-prometheus-stack -f kube-prometheus-stack/values.d/limnad.yaml -f kube-prometheus-stack/values.d/limnad.secret.yaml --atomic --debug
-helm upgrade --install nginx-ingress -n clusterwide ingress-nginx/ingress-nginx -f nginx-ingress/values.d/limnad.yaml -f nginx-ingress/values.d/limnad.secret.yaml --atomic --debug
-helm upgrade --install certs -n clusterwide certs/certs -f certs/values.d/limnad.yaml -f certs/values.d/limnad.secret.yaml --atomic --debug
 helm upgrade --install postgresql -n db bitnami/postgresql -f postgresql/values.d/limnad.yaml -f postgresql/values.d/limnad.secret.yaml --atomic --debug
 helm upgrade --install -n phantom hackmd stable/hackmd -f hackmd/values.d/limnad.yaml -f hackmd/values.d/limnad.secret.yaml --atomic --debug
-helm upgrade --install -n db minio minio/minio -f minio/values.d/limnad.yaml --atomic --debug
 helm upgrade --install kix -n phantom website-kix-co-il
 helm upgrade --install binaryvision -n phantom website-binaryvision-static
 helm upgrade --install tlo -n phantom website-bv-tlo
