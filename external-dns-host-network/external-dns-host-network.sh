6f1426543e00ca08e222b8b67e1a0ba856165b5e
Anton Wolkov <anton.wolkov@toptal.com>
2024-09-17 10:52:49 +0300

    external-dns ipv6 support

--- external-dns-host-network/external-dns-host-network.sh
+++ external-dns-host-network/external-dns-host-network.sh
@@ -5,6 +5,7 @@ set -e
 echo starting external-dns-host-network...
 
 touch external-ips-last.txt
+touch internal-ips-last.txt
 
 while true; do
 
@@ -15,81 +16,23 @@ touch /tmp/healthy
 kubectl describe node | grep -oE 'ExternalIP:.*' | cut -f2 -d':' | \
   grep -oE '[0-9.]+\.[0-9.]+' > external-ips.txt || continue
 
-diff -q external-ips.txt external-ips-last.txt && continue
+kubectl describe node | grep -oE 'InternalIP:[ :0-9a-f]*$' | cut -f2- -d':' | \
+  grep -oE '[^ ]+' | sed 's/:0:0$/::/' > internal-ips.txt || continue
 
-echo "new external ip addresses detected:"
+diff -q external-ips.txt external-ips-last.txt && \
+  diff -q internal-ips.txt internal-ips-last.txt && \
+  continue
+
+diff -q external-ips.txt external-ips-last.txt || echo "new external ip addresses detected"
+diff -q internal-ips.txt internal-ips-last.txt || echo "new internal ip addresses detected"
 cat external-ips.txt
-
-if [ -n "$LINODE_CLI_TOKEN" ]; then
-
-  echo linode token detected, updating LINODE_TAG="$LINODE_TAG" \
-    SPECIAL_DOMAIN="$SPECIAL_DOMAIN" SPECIAL_TARGET="$SPECIAL_TARGET"
-
-  linode-cli domains list --format 'id' --tag "$LINODE_TAG" --json \
-    | python -m json.tool | grep '"id"' | cut -f2 -d':' | grep -oE '[^ ]+' \
-    | tee domains.txt
-
-  grep -v '^ *#' < domains.txt | while IFS= read -r domain; do
-
-    echo "checking $domain"
-
-    linode-cli domains records-list "$domain" --format 'id,type,name,target' \
-      --json | tee domain-info.json
-
-    grep -oE '\{"id"[^}]+' domain-info.json \
-      | grep '"type": "A", "name": "", "target": "' \
-      | tee domain-targets.txt
-
-    if [ "$domain" == "$SPECIAL_DOMAIN" ]; then
-      grep -oE '\{"id"[^}]+' domain-info.json \
-        | grep "\"type\": \"A\", \"name\": \"$SPECIAL_TARGET\", \"target\": \"" \
-        | tee -a domain-targets.txt
-    fi
-
-    grep -v '^ *#' < external-ips.txt | while IFS= read -r ip; do
-
-      echo "checking external ip $ip"
-
-      grep -v "$ip" domain-targets.txt > domain-targets.txt1 || true
-      mv domain-targets.txt1 domain-targets.txt || true
-
-      grep -q "\"type\": \"A\", \"name\": \"\", \"target\": \"$ip\"" \
-        domain-info.json && continue
-
-      echo "adding $ip to root"
-
-      linode-cli domains records-create "$domain" --type A --name "" \
-        --target "$ip" --ttl_sec 300
-
-      if [ "$domain" == "$SPECIAL_DOMAIN" ]; then
-        grep "\"type\": \"A\", \"name\": \"$SPECIAL_TARGET\", \"target\": \"$ip\"" \
-          -q domain-info.json && continue
-        echo "adding $ip to $SPECIAL_DOMAIN"
-        linode-cli domains records-create "$domain" --type A --target "$ip" \
-          --name "$SPECIAL_DOMAIN"  --ttl_sec 300
-      fi
-
-      echo added
-
-    done
-
-    grep id --color domain-targets.txt || true
-
-    grep -oE '"id": [0-9]+' domain-targets.txt | cut -f2 -d':' \
-      | grep -oE '[0-9]+' | while IFS= read -r record; do
-      echo "deleting $domain record $record"
-      linode-cli domains records-delete "$domain" "$record"
-    done
-
-  done
-
-fi  # [ -n "$LINODE_CLI_TOKEN" ]
+cat internal-ips.txt
 
 if [ -n "$CLOUDFLARE_TOKEN" ]; then
 
   echo cloudflare token detected
 
-  curl -X GET "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/dns_records?name=${CLOUDFLARE_NAME}" \
+  curl -X GET "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/dns_records?name=${CLOUDFLARE_NAME}&type=A" \
     -H "Content-Type:application/json" \
     -H "Authorization: Bearer ${CLOUDFLARE_TOKEN}" \
     | jq -r '.result[] | "\(.id) \(.content)"' \
@@ -115,10 +58,37 @@ if [ -n "$CLOUDFLARE_TOKEN" ]; then
         -H "Authorization: Bearer ${CLOUDFLARE_TOKEN}"
   done
 
+  curl -X GET "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/dns_records?name=${CLOUDFLARE_NAME}&type=AAAA" \
+    -H "Content-Type:application/json" \
+    -H "Authorization: Bearer ${CLOUDFLARE_TOKEN}" \
+    | jq -r '.result[] | "\(.id) \(.content)"' \
+    | tee entries.txt
+
+  grep -v '^ *#' < internal-ips.txt | while IFS= read -r ip; do
+    echo "checking if $ip needs to be added"
+    grep "$ip" entries.txt || \
+      curl -s -D - -X POST "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/dns_records" \
+        -H "Content-Type:application/json" \
+        -H "Authorization: Bearer ${CLOUDFLARE_TOKEN}" \
+        --data "{\"type\":\"AAAA\",\"name\":\"${CLOUDFLARE_NAME}\",\"content\":\"$ip\",\"ttl\":${CLOUDFLARE_TTL},\"proxied\":false}"
+  done
+
+  # shellcheck disable=SC2094
+  grep -oE '\b[^ ]+$' < entries.txt | while IFS= read -r ip; do
+    # shellcheck disable=SC2094
+    id=$(grep "$ip" entries.txt | grep -oE '^[^ ]+')
+    echo "checking if $ip needs to be removed"
+    grep "$ip" internal-ips.txt || \
+      curl -s -D - -X DELETE "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE}/dns_records/${id}" \
+        -H "Content-Type:application/json" \
+        -H "Authorization: Bearer ${CLOUDFLARE_TOKEN}"
+  done
+
 fi  # [ -n "$CLOUDFLARE_TOKEN" ]
 
 echo Done!
 
 cp external-ips.txt external-ips-last.txt
+cp internal-ips.txt internal-ips-last.txt
 
 done
